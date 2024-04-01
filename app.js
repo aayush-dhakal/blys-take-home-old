5cc765eea946af10273d9747b9c5f9029926a675
aayush-dhakal <aayushdhakal77@gmail.com>
2024-04-02 00:09:25 +0545

    fixed typo

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
@@ -20,7 +19,7 @@ app.post("/api/verify", (req, res) => {
 
 app.use(express.static("frontend/build"));
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 );
 
 module.exports = app;
